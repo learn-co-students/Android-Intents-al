a0d923651610a2a8422a87b0409195986dcb2865
Brandon Romano <brandon@brandons-mbp.home>
2014-10-19 22:21:38 -0400

    Solution

--- app/src/main/java/com/flatironschool/intents/activities/FirstActivity.java
+++ app/src/main/java/com/flatironschool/intents/activities/FirstActivity.java
@@ -1,15 +1,15 @@
 package com.flatironschool.intents.activities;
 
+import android.content.Intent;
+
 import com.flatironschool.intents.abstracts.FirstActivityAbs;
 
 public class FirstActivity extends FirstActivityAbs {
 
-    /**
-     * TODO: Use an intent to switch to the second activity
-     */
     protected void onContinueButtonClick()
     {
-
+        Intent myIntent = new Intent(this, SecondActivity.class);
+        startActivity(myIntent);
     }
 
 }
--- app/src/main/java/com/flatironschool/intents/activities/SecondActivity.java
+++ app/src/main/java/com/flatironschool/intents/activities/SecondActivity.java
@@ -1,18 +1,17 @@
 package com.flatironschool.intents.activities;
 
+import android.content.Intent;
+
 import com.flatironschool.intents.abstracts.SecondActivityAbs;
 
 public class SecondActivity extends SecondActivityAbs {
 
-    /**
-     * TODO: Create an intent to shoot us off to the third activity
-     *
-     * Use ThirdActivity.EXTRA_ANYTHING_TEXT and ThirdActivity.EXTRA_COLOR
-     * as keys for the extras to be passed
-     */
     protected void continueOnClick(String anythingInput, String colorInput)
     {
-
+        Intent myIntent = new Intent(this, ThirdActivity.class);
+        myIntent.putExtra(ThirdActivity.EXTRA_ANYTHING_TEXT, anythingInput);
+        myIntent.putExtra(ThirdActivity.EXTRA_COLOR, colorInput);
+        startActivity(myIntent);
     }
 
 }
